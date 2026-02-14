c109af803e533304263481042d3d276cdd6e652c
David Triminio <davidtriminio21@gmail.com>
2026-02-13 22:31:04 -0600

    chore(client): initialize frontend app

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <span className="flex items-center">
+      <span className="h-px flex-1 bg-gray-300 dark:bg-gray-600"></span>
+
+      <span className="shrink-0 px-4 text-gray-900 dark:text-gray-600">X-perience</span>
+
+      <span className="h-px flex-1 bg-gray-300 dark:bg-gray-600"></span>
+    </span>
+  );
+};
+
+export default App;
