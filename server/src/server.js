f8cd9811dfd9bebeac94448d4ac7a5f50c80a863
David Triminio <davidtriminio21@gmail.com>
2026-02-16 19:01:51 -0600

    feat(auth-server): add Clerk middleware to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 import express from "express"
 import {ENV} from "./config/env.js"
 import {connectDB} from "./db/db.js";
+import {clerkMiddleware} from "@clerk/express";
 
 const app = express()
 
 app.use(express.json())
+app.use(clerkMiddleware())
 app.get('/', (req, res) => {
     res.send("Connected")
 })
