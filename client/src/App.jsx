bbbeb366bf7faccfc25780cd6777faccdb5596c7
David Triminio <davidtriminio21@gmail.com>
2026-02-20 21:31:31 -0600

    feat(auth-client): add protected auth routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {useAuth} from '@clerk/clerk-react';
 import AuthPage from "./pages/AuthPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import {Routes, Route} from "react-router-dom";
+import {Navigate} from "react-router";
 
 
 const App = () => {
@@ -9,8 +10,8 @@ const App = () => {
     if(!isLoaded) return null
     return (
         <Routes>
-            <Route path='/auth' element={<AuthPage/>}/>
-            <Route path='/' element={<HomePage/>}/>
+            <Route path='/' element={isSignedIn ? <HomePage/> : <Navigate to={'/auth'} replace/>}/>
+            <Route path='/auth' element={!isSignedIn ? <AuthPage/> : <Navigate to={'/'} replace/>}/>
         </Routes>
     );
 };
