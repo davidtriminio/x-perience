7e98684481ba8cff345860e029664b251be2383e
David Triminio <davidtriminio21@gmail.com>
2026-02-23 21:31:15 -0600

    feat(home): implement Post Model logic

---
+++ server/src/models/post.model.js
@@ -0,0 +1,9 @@
+﻿import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    authorId: {type: String, required: true},
+    authorName: {type: String},
+    content: {type: String, required: true, maxLength: 500}
+}, {timestamps: true})
+
+export const postModel = mongoose.model("Post", postSchema)
