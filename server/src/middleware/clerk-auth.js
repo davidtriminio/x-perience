4025d9d8edbfe68911ef4e9c791a01dac570b409
David Triminio <davidtriminio21@gmail.com>
2026-02-24 19:48:22 -0600

    refactor(middleware): improve clerk auth

---
+++ server/src/middleware/clerk-auth.js
@@ -0,0 +1,16 @@
+﻿import {authenticateRequest} from "@clerk/express";
+
+const requireAuth = (req, res, next) => {
+    try {
+        if (!req) throw  new Error("Request no definido.")
+        const {userId, sessionId} = authenticateRequest(req)
+        if (!userId)return res.status(401).json({error: 'No autorizado. Usuario Inválido.'})
+        req.user = {userId, sessionId}
+        next();
+    } catch (e) {
+        console.log("Error en autenticación:", e)
+        res.status(401).json({error: "No autorizado."})
+    }
+}
+
+export default requireAuth;
