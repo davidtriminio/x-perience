89ecf5cd11a2686dac20c58394dfab28a03b0af5
David Triminio <davidtriminio21@gmail.com>
2026-02-24 20:00:49 -0600

    feat(home): integrte posts API

---
+++ client/src/api/posts.js
@@ -0,0 +1,12 @@
+﻿import axios from "axios"
+import {ENV} from "../config/env.js"
+
+const API = `${ENV.API_URL}/api/posts`
+
+export const getPosts = () => axios.get(API)
+
+export const createPost = (data, token) => axios.post(API, data, {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
