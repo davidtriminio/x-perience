33c68a47cdd877a72fd1034769a46b17b12b1c5b
David Triminio <davidtriminio21@gmail.com>
2026-02-24 22:25:01 -0600

    feat(posts): add CreatePost skeleton

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,40 @@
+﻿import React, {useState} from 'react'
+import {useAuth} from "@clerk/clerk-react";
+import {createPost} from "../api/posts.js";
+import {useNavigate} from "react-router";
+
+const CreatePost = ({onPostCreated}) => {
+    const [content, setContent] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const {getToken} = useAuth()
+    const navigate = useNavigate()
+    const submit = async () => {
+        if (isLoading || !content.trim()) return;
+        
+        try {
+            setIsLoading(true)
+            const token = await getToken()
+            await createPost({content}, token)
+            setContent("")
+            if (onPostCreated){
+                onPostCreated()
+            }
+            navigate('/')
+        } catch (error) {
+            console.error("Error creando post:", error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <div>
+            <textarea
+            value={content}
+            onChange={e=>setContent(e.target.value)}
+            placeholder="¿Qué está pasando?"/>
+            <button onClick={submit} disabled={isLoading || !content.trim()}>{isLoading ? "Publicando..." : "Publicar"}</button>
+        </div>
+    )
+}
+export default CreatePost
