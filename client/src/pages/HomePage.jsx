a67bea91c335a217e62c86ce9db4cd99afc9637e
David Triminio <davidtriminio21@gmail.com>
2026-02-24 22:23:50 -0600

    feat(home): add posts skeleton

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,35 @@
-﻿import React from 'react'
+﻿import React, {useEffect, useState} from 'react'
+import {useAuth} from "@clerk/clerk-react";
+import {getPosts} from "../api/posts.js";
+import CreatePost from "./CreatePost.jsx";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState([]);
+    const {getToken} = useAuth()
+
+    useEffect(() => {
+        loadPosts()
+    }, []);
+
+    const loadPosts = async () => {
+        const res = await getPosts()
+        console.log(res.data)
+        setPosts(res.data)
+    }
+    
     return (
-        <div>HomePage</div>
+        <div>
+            <CreatePost onPostCreated={loadPosts}/>
+            <h2>Feed</h2>
+            {posts.map(p =>(
+                <div key={p._id}>
+                    <strong>{p.authorName}</strong>
+                    <p>{p.content}</p>
+                    <small>{new Date(p.createdAt).toLocaleString()}</small>
+                    <hr/>
+                </div>
+            ))}
+        </div>
     )
 }
 export default HomePage
